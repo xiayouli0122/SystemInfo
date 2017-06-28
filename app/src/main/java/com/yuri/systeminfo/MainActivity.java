12e580bc181605a46bfcfa8c740d7559b59b0670
Yuri <shinaman@163.com>
2017-06-28 22:28:01 +0800

    modify

--- app/src/main/java/com/yuri/systeminfo/MainActivity.java
+++ app/src/main/java/com/yuri/systeminfo/MainActivity.java
@@ -10,6 +10,7 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -81,10 +82,11 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_open).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String baseUrl = "https://www.baidu.com";
+                String baseUrl = "https://m.baidu.com";
                 String qudao = "/from=844b";
                 String keyword = "/s?word=ip";
-                String guanggao = "&sa=tb&ts=0&t_kt=0&ie=utf-8&rsv_t=c2eaKwziCjrrEC9Ar4vQxouAgsELwXwjEBY1YwzHvpQQrl%252Bh9Jp9&rsv_pq=14318853138871529188";
+                String guanggao = "";
+//                String guanggao = "&sa=tb&ts=0&t_kt=0&ie=utf-8&rsv_t=c2eaKwziCjrrEC9Ar4vQxouAgsELwXwjEBY1YwzHvpQQrl%252Bh9Jp9&rsv_pq=14318853138871529188";
 //                String guanggao = "&sa=tb&ts=6651294&t_kt=90&ie=utf-8&rsv_t=130bsjAIWB%252F%252F3HE40pdZEVGZpswrvZkGCSYnrYh9I0e7z%252BIUdfpAnSnvEg&ms=1&rsv_pq=7130147884630772087&ss=100&t_it=1&rqlang=zh&rsv_sug4=6814&inputT=5896&oq=阿里收购联华超市";
                 Intent intent = new Intent();
                 String url = baseUrl + qudao + keyword + guanggao;
@@ -92,6 +94,28 @@ public class MainActivity extends AppCompatActivity {
                 intent.setData(Uri.parse(url));
                 intent.setAction(Intent.ACTION_VIEW);
                 startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_info).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getInfo();
+            }
+        });
+
+        findViewById(R.id.btn_imei).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+//                    Runtime.getRuntime().exec("input tap 250 300");
+//                    Runtime.getRuntime().exec("setprop microvirt.imei 222222222222222222");
+                    Runtime.getRuntime().exec("setprop microvirt.linenum 13666666666");
+//                    Runtime.getRuntime().exec(" date -s 20150312.123234");
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -188,26 +212,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getInfo() {
+        String infostr = "";
         //获取IMEI
         TelephonyManager telephonyManager = (TelephonyManager) this.getSystemService(TELEPHONY_SERVICE);
 
         String deviceId = telephonyManager.getDeviceId();
         Log.d(TAG, "deviceId: " + deviceId);
+        infostr += "deviceId=" + deviceId + "\n";
 
         String number = telephonyManager.getLine1Number();
         Log.d(TAG, "number: " + number);
+        infostr += "number=" + number + "\n";
 
         String deviceSoftwareVersion = telephonyManager.getDeviceSoftwareVersion();
         Log.d(TAG, "deviceSoftwareVersion: " + deviceSoftwareVersion);
+        infostr += "deviceSoftwareVersion=" + deviceSoftwareVersion + "\n";
 
         String model = Build.MODEL;
         Log.d(TAG, "model: " + model);
+        infostr += "model=" + model + "\n";
 
         String release = Build.VERSION.RELEASE;
         Log.d(TAG, "release version: " + release);
+        infostr += "version=" + release + "\n";
 
         int sdk = Build.VERSION.SDK_INT;
         Log.d(TAG, "SDK: " + sdk);
+        infostr += "sdk=" + sdk + "\n";
 
         //在wifi未开启状态下，仍然可以获取MAC地址，但是IP地址必须在已连接状态下否则为0
         String ip = null;
@@ -219,7 +250,9 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "MacAddress: " + getMac());
         Log.d(TAG, "IP: " + ip);
-
+        infostr += "MacAddress=" + getMac() + "\n";
+        infostr += "IP=" + ip + "\n";
+        mTextView.setText(infostr);
     }
 
     public String int2ip(long ipInt) {
